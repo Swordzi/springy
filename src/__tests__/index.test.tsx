9132c9369d411882c439b9b96c26a95bfa1e7c23
Swordzi <github@sword.mozmail.com>
2022-06-02 19:06:51 +0300

    Misc.

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Main from './index';
+import Main from '../index';
 
 test('renders learn react link', () => {
   render(<Main />);
