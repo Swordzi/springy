2ee808b0572a434080ee3530b46981b1224bf52a
Swordzi <github@sword.mozmail.com>
2022-06-03 14:43:31 +0300

    Added some text to the navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import "./Styles/Navbar.scss";
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Root</Link>
+        <header className="Navbar-header">
+            <div className="left">
+                <p>Sword.today</p>
 
-                </li>
-                <li>
-                    <Link to="/Docs">Docs</Link>
-                </li>
-            </ul>
-        </nav>
+            </div>
+
+            <nav className="mid">
+                <ul className="navbar">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/Docs">Docs</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="right">
+                <a href="#">Welcome</a>
+            </div>
+        </header>
     );
 }
 
