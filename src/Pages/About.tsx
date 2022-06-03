23db889ece80b0527deadb11c909806c730cfede
Swordzi <github@sword.mozmail.com>
2022-06-03 14:44:00 +0300

    Started working on an about page

---
+++ src/Pages/About.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+
+function About() {
+    return (
+        <div className="about">
+
+
+
+
+
+        </div>
+
+
+    );
+}
+
+export default About;
